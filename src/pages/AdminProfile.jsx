05073a13c8d9d213f6e5a5114047392c72151b4c
Ernest_Torto <111897917+ettorto@users.noreply.github.com>
2023-12-01 10:33:07 +0000

    Add files via upload

---
+++ src/pages/AdminProfile.jsx
@@ -0,0 +1,10 @@
+import { Layout } from "../components/Layout";
+import { Profile } from "../components/Profile";
+
+export const AdminProfile = () => {
+  return (
+    <>
+      <Layout content={<Profile/>} />
+    </>
+  );
+};
