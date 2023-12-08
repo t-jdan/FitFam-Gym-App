edff03356fe59181971d9c403a4f572cbf3b8a7e
Ernest_Torto <111897917+ettorto@users.noreply.github.com>
2023-12-08 10:48:32 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Sidebar from "./components/SideBar";
+import { HorizontalCard } from "./components/HorizontalCard";
+import { Table } from "./components/Table";
+import { Table1} from "./components/Table1";
+import { AdminDashboard } from "./pages/AdminDashboard";
+import { SignUp } from "./pages/SignUp";
+import { Login } from "./pages/Login";
+import { AdminProfile } from "./pages/AdminProfile";
+import TestPage from "./pages/Test";
+import {AdminEquipment} from "./pages/AdminEquipment";
+import {AdminTrainer} from "./pages/AdminTrainer";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<AdminProfile />} />
+          <Route path="/dashboard" element={<AdminDashboard />} />
+          <Route path="/test" element={<TestPage />} />
+          <Route path="/equipment" element={<AdminEquipment />} />
+          <Route path="/trainers" element={<AdminTrainer />} />
+
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
