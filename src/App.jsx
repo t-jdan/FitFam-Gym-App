bc5562793c23ad28811d8e7e93dc006c54245307
Ernest_Torto <111897917+ettorto@users.noreply.github.com>
2023-12-11 15:24:57 +0000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,51 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { AdminDashboard } from "./pages/Admin/AdminDashboard";
+import { AdminDashboard } from "./pages/admin/AdminDashboard";
 import { SignUp } from "./pages/SignUp";
 import { Login } from "./pages/Login";
-import { AdminProfile } from "./pages/Admin/AdminProfile";
-import TestPage from "./pages/Test";
-import {AdminEquipment} from "./pages/Admin/AdminEquipment";
-import {AdminTrainer} from "./pages/Admin/AdminTrainer";
-import { TrainerDashboard } from "./pages/Trainers/TrainerDashboard";
-import {TrainerProfile} from "./pages/Trainers/TrainerProfile";
-import {TrainerAttendance} from "./pages/Trainers/TrainerAttendance";
-import {TrainerCompetition} from "./pages/Trainers/TrainerCompetition";
+import { AdminProfile } from "./pages/admin/AdminProfile";
+import { AdminEquipment } from "./pages/admin/AdminEquipment";
+import { AdminTrainer } from "./pages/admin/AdminTrainer";
+import { TrainerDashboard } from "./pages/trainer/TrainerDashboard";
+import { TrainerProfile } from "./pages/trainer/TrainerProfile";
+import { TrainerAttendance } from "./pages/trainer/TrainerAttendance";
+import { TrainerCompetition } from "./pages/trainer/TrainerCompetition";
+import { SchedulePage } from "./pages/trainer/Schedule";
+import { HomePage } from "./pages/Homepage";
+import { AboutPage } from "./pages/AboutPage";
+import {ViewAttendance} from "./pages/admin/ViewAttendance";
+import { Report } from "./pages/admin/Report";
 import { StudentDashboard } from "./pages/Student/StudentDashboard";
 import { StudentCompetition } from "./pages/Student/StudentCompetition";
-import { TrainerSchedule } from "./pages/Trainers/TrainerSchedule";
+import { TrainerSchedule } from "./pages/trainer/TrainerSchedule";
+
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          {/* Sign in and Log in */}
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-
-          {/* Admin */}
           <Route path="/profile" element={<AdminProfile />} />
           <Route path="/dashboard" element={<AdminDashboard />} />
           <Route path="/equipment" element={<AdminEquipment />} />
           <Route path="/trainers" element={<AdminTrainer />} />
-
-          {/* Student */}
-          <Route path="/stud_dashboard" element={<StudentDashboard />} />
-          <Route path="/stud_competition" element={<StudentCompetition />} />
-          <Route path="/trainer_schedule" element={<TrainerSchedule />} />
-          <Route path="/stud_complaint" element={<TrainerSchedule />} />
-          <Route path="/stud_profile" element={<TrainerSchedule />} />
-          <Route path="/test" element={<TestPage />} />
-
-          {/* Trainers */}
+          <Route path="/attendance" element={<ViewAttendance />} />
           <Route path="/trainerdashboard" element={<TrainerDashboard />} />
           <Route path="/trainerprofile" element={<TrainerProfile />} />
           <Route path="/trainerattendance" element={<TrainerAttendance />} />
           <Route path="/competition" element={<TrainerCompetition />} />
+          <Route path="/schedule" element={<SchedulePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/report" element={<Report />} />
+           {/* Student */}
+           <Route path="/stud_dashboard" element={<StudentDashboard />} />
+          <Route path="/stud_competition" element={<StudentCompetition />} />
+          <Route path="/trainer_schedule" element={<TrainerSchedule />} />
+          <Route path="/stud_complaint" element={<TrainerSchedule />} />
+          <Route path="/stud_profile" element={<TrainerSchedule />} />
         </Routes>
       </Router>
     </>
