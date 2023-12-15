cd9d68f9259143b3e11f5c4a0946f801870dad28
Jordan Teye <teyejordan@gmail.com>
2023-12-15 02:23:10 +0000

    updated favicon and tab name. Created load screen

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,6 @@
+// LoadingSpinner.jsx
+import React from 'react';
+
+export const LoadingSpinner = () => {
+  return <div className="loading-spinner">Loading...</div>;
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
-
+import { LoadingSpinner } from '../components/LoadingSpinner';
 import {LandingPage} from '../components/Landingpage';
+import React, {useState, useEffect} from 'react'
 
 export function HomePage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [])
   return (
     <div className="App">
       <LandingPage />
